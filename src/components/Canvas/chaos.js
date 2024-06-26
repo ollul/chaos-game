b014e06438e57112514af3d420255d19aa255bfb
ollul <46057440+ollul@users.noreply.github.com>
2024-06-26 21:30:22 +0300

    Update chaos.js

--- src/components/Canvas/chaos.js
+++ src/components/Canvas/chaos.js
@@ -30,7 +30,7 @@ export class Chaos {
         let gradient = new Set();
 
         gradients.forEach(([[rs, gs, bs], [re, ge, be]]) => {
-            const c = 0.01
+            const c = 0.0001
             const dr = re - rs;
             const dg = ge - gs;
             const db = be - bs;
@@ -38,7 +38,7 @@ export class Chaos {
             for(let i = 0; i <=  Math.floor(1 / c); i++) gradient.add('#' + toHex(dr, rs, c * i) + toHex(dg, gs, c * i) + toHex(db, bs, c * i));            
         });
         
-        const div = Math.ceil(gradient.size / points.length);
+        const div = Math.round(gradient.size / points.length);
         return Array.from(gradient.values()).filter((_, i) => !(i % div)).map((color, idx) => ({...points[idx], color}));
     }
     
@@ -52,6 +52,12 @@ export class Chaos {
         this.initialPoints = this.mapColorsToPoints(initialPoints);
         this.lastPoint = {x: Math.floor(Math.random() * this.width), y: Math.floor(Math.random() * this.height), color: '#000'};
         this.drawPoints(this.initialPoints, 5);  
+        this.sw = this.width;
+        this.sh = this.height;
+        this.kx = 1;
+        this.ky = 1;
+        this.workedPoints = this.initialPoints.map(p => ({...p}));
+        this.points = [];
     }
 
     randomPoint() {
@@ -67,7 +73,6 @@ export class Chaos {
     }
 
     drawPoint({x, y, color}, size = 1) {
-        console.log(this);
         this.ctx.fillStyle = color;
         this.ctx.beginPath();
         this.ctx.arc(x, y, size, 0, 2 * Math.PI);
@@ -78,9 +83,15 @@ export class Chaos {
     render() {
         const points = Array.from({length: this.drawPointsForStep}).map(() => {
             this.lastPoint = this.makeNextPoint(this.randomPoint());
-            return this.lastPoint;
+            this.points.push(this.lastPoint);
+            return {
+                color: this.lastPoint.color,
+                x: this.lastPoint.x * this.kx,
+                y: this.lastPoint.y * this.ky
+            }
         });
-        this.drawPoints(points)
+        console.log(this.points.length);
+        this.drawPoints(points);
     }
 
     start({ r } = { r: this.r}) {
@@ -99,21 +110,52 @@ export class Chaos {
     }
 
     reset() {
+        window.clearTimeout(this.debounceId);
         clearInterval(this.renderIntervalId);
         this.initialPoints = [];
         this.clear();
     }
+    
+    resize() {
+       window.clearTimeout(this.debounceId);
+       this.debounceId = setTimeout(() => {
+        this.rs = true;
+        this.kx = this.width / this.sw;
+        this.ky = this.height / this.sh;
 
+        this.stretchCanvas();
+        this.clear();
+        this.drawPoints(this.initialPoints.map(p => ({
+            ...p,
+            x: p.x * this.kx,
+            y: p.y * this.ky
+        })), 5);
+        const np = this.points.map(p => ({
+            ...p,
+            x: p.x * this.kx,
+            y: p.y * this.ky
+        }));
+
+        this.drawPoints(np);
+       }, 100);
+    }
     
     constructor(canvas) {
+        this.debounceId = null;
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
         this.stretchCanvas();
         this.initialPoints = [];
+        this.workedInitialPoints = [];
         this.r = .5;
         this.drawPointsForStep = 1000;
         this.renderIntervalMs = 1000;
         this.lastPoint = {x: 0, y: 0};
-        this.renderIntervalId = null;        
+        this.renderIntervalId = null;    
+        this.points = []; 
+        this.kx = 1;
+        this.ky = 1;   
+        this.ws = 0;
+        this.hs = 0;
     }
 }
